d96bf0ee5d5a701e8f1277a428bbee402ae107c7
LeonardoZapata <lzapata@hircasa.com>
2024-01-14 08:10:55 -0600

    correcto2

--- ATENEA/Controllers/PermisosController.cs
+++ ATENEA/Controllers/PermisosController.cs
@@ -9,20 +9,33 @@ using Microsoft.Extensions.Logging;
 using ATENEA.Models;
 using CapaEntidad;
 using CapaNegocio;
+using Microsoft.Identity.Web;
 
 namespace WebApp.Controllers
 {
     [Authorize]
     public class PermisosController : Controller
     {
+        private readonly ITokenAcquisition _tokenAcquisition;
         private readonly ILogger<PermisosController> _logger;
        
         
 
-        public PermisosController(ILogger<PermisosController> logger)
+        public PermisosController(ILogger<PermisosController> logger,ITokenAcquisition tokenAcquisition)
         {
             _logger = logger;
+            _tokenAcquisition = tokenAcquisition;
         }
+
+        public async Task<IActionResult> Index2()
+    {
+        string[] scopes = { "user.read" };
+        string accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+        
+        // Usa el accessToken aquí
+
+        return View();
+    }
 
         public IActionResult Index()
         {   
